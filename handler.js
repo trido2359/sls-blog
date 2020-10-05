8fbfdbe84b3ff0b93e641bb478459bf96a46c517
trido2359 <tri.do@2359media.com>
2020-10-05 23:12:20 +0700

    update3

--- handler.js
+++ handler.js
@@ -1,5 +1,8 @@
 
 module.exports.helloWorld = async (event, context) => {
     console.log('hello');
-    return "hello"
+    return {
+        statusCode: 200,
+        body: 'Hello 123'
+    };
 };
