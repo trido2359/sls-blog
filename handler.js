eae44878426fb1f04c78ca60a52232eaa6cdf193
trido2359 <tri.do@2359media.com>
2020-10-07 21:32:40 +0700

    deploy

--- handler.js
+++ handler.js
@@ -1,3 +1,42 @@
+const AWS = require('aws-sdk');
+
+const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({
+    apiVersion: '2016-04-18',
+    region: 'ap-southeast-1',
+});
+
+module.exports.registerAccount = async (event, context) => {
+    try {
+        const { body } = event;
+        const { email, password } = JSON.parse(body);
+        const params = {
+            ClientId: process.env.COGNITO_CLIENT_ID, /* required */
+            Password: password, /* required */
+            Username: email, /* required */
+          };
+          
+        const result = await cognitoIdentityServiceProvider.signUp(params).promise();
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify({ accountId: result.UserSub})
+        };
+    } catch (err) {
+        const { code } = err;
+        switch (code) {
+            case 'UsernameExistsException':
+                return {
+                    statusCode: 400,
+                    body: 'UsernameExistsException'
+                };
+        }
+        
+        return {
+            statusCode: 500,
+            body: 'Server err'
+        };
+    }
+}
 
 module.exports.helloWorld = async (event, context) => {
     console.log('hello1');
